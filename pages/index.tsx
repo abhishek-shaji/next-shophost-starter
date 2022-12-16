8417270cf8d719142f8b84853da50406d01cf641
Abhishek Shaji <ashaji@Abhisheks-Mac-Pro.local>
2022-12-17 00:07:38 +0100

    feat(setup-nxt-translate): setup next translate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import useTranslation from 'next-translate/useTranslation';
+
 export default function Home() {
+  const { t } = useTranslation('common');
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">{t('hello')}</h1>
     </>
   );
 }
