9d9c4d36bf9af2f33b988c1642fe6d2a7b4466e4
Abhishek Shaji <ashaji@Abhisheks-Mac-Pro.local>
2022-12-17 17:11:28 +0100

    feat(setup-shophost): setup shophost sdk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
+import Shophost from '@abhishek-shaji/shophost-react-sdk';
 import type { AppProps } from 'next/app';
+import useTranslation from 'next-translate/useTranslation';
 
 import '../styles/globals.css';
+import '@abhishek-shaji/shophost-react-sdk/dist/main.css';
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  const { lang }: any = useTranslation();
+  const merchantId = process.env.NEXT_PUBLIC_SHOPHOST_MERCHANT_ID;
+
+  if (!merchantId) {
+    throw new Error('Missing merchant ID');
+  }
+
+  return (
+    <Shophost.Provider merchantId={merchantId} locale={lang}>
+      <Component {...pageProps} />
+    </Shophost.Provider>
+  );
+};
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
+import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
 
+import i18nConfig from '../i18n.json';
+
+const { locales } = i18nConfig;
+
 export default function Home() {
-  const { t } = useTranslation('common');
+  const { t, lang } = useTranslation('common');
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">{t('hello')}</h1>
+      <ul className="border-2 absolute top-2 right-2">
+        {locales.map((locale) => (
+          <li key={locale} className="border-b p-2 bg-black text-white">
+            <Link href="/" locale={locale} passHref>
+              {locale}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <main className="flex items-center justify-center h-screen bg-gray-100">
+        <div className="flex flex-col items-center text-center">
+          <h1 className="font-bold text-9xl text-gray-800 mb-4">
+            {t('hello')} - {lang.toUpperCase()}
+          </h1>
+        </div>
+      </main>
     </>
   );
 }
